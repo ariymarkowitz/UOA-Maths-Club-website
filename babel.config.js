ab2eeb57eed9c77a655eac2472019ff01048a40d
Ari Markowitz <ariymarkowitz@gmail.com>
2019-04-21 21:27:51 +1200

    Install potentially useful libraries

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,9 @@ module.exports = (api) => {
     ['@babel/preset-env', { targets: { browsers: '> 1%, not ie 11' } }],
     ['@babel/preset-react', { development: api.env('development') }]
   ];
-  const plugins = ['@babel/plugin-syntax-dynamic-import'];
+  const plugins = [
+    '@babel/plugin-syntax-dynamic-import',
+    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }]
+  ];
   return { presets, plugins };
 };
